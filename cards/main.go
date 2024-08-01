5fd83d8681d509ed060719f307b24c2d906cb736
Erik Burgess <erburges@cisco.com>
2024-08-01 11:54:08 -0700

    Initial cards

---
+++ cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{
+		newCard(), "Ace of Diamonds",
+	}
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of diamonds"
+
+}
